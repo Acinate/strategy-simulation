851be5fd261f8032488a8cbcf780fc385f639925
Acinate <jason.efthimi@gmail.com>
2021-11-02 11:45:32 -0400

    updated simulation

--- src/Simulation.java
+++ src/Simulation.java
@@ -2,36 +2,69 @@ import java.text.NumberFormat;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
+// This strategy uses a Triple-OCO Bracket Order. See: https://tlc.thinkorswim.com/center/howToTos/thinkManual/Trade/Active-Trader/AT-Entering-Orders
+// Stop loss is set to 25% for all contracts (we use 30% for slippage / market orders)
+// Take profits are set at 75%, 100%, and 125% which gives us around a 25% - 100% ROC
+
+// We start with an account size of $250. Increasing this initial balance doesn't improve results (actually makes it worse)
+// Anytime we reach a new profitLevel (e.g. 1000, 2000, etc.) we take half of that value and add permanently to our bank-roll
+// After reaching this profitLevel, we increase our level to the next. (e.g. tradeBalance hit 1000, now we target 2000)
+
+// The only time we return to a previous level is when the tradeBalance falls below $80. This is where we have
+// to add more money from our bank-roll. We always only add $250 as this tradeBalance is very volatile.
+// This protects our accounts from the inevitable draw-downs. Worst case scenario we lose 10 trades in a row
+// and lose $250. However, we still have a massive bank-roll that will supply us with unlimited $250 initial balances.
 public class Simulation {
     private static double[] profitLevels = new double[]{1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 256000, 512000, 1024000};
 
     public static void main(String[] args) {
-//        runManySimulations();
-        simulateHomeRunStrategy();
+//        runManySimulations(25000, 0.029, 0.031, 0.009, 0.011, false);
+//        runManySimulations(25000, 0.03, 0.09, 0.01, 0.03, false);
+//        runManySimulations(25000, 0.06, 0.18, 0.02, 0.06, false);
+//        runManySimulations(250, 0.30, 0.75, 0.10, 0.25, true);
+//        simulateStockStrategy();
+//        simulateOptionsStrategy();
+        simulateCryptoStrategy();
+//        loseUntilZero();
     }
 
-    static void simulateHomeRunStrategy() {
-        // This strategy uses a Triple-OCO Bracket Order. See: https://tlc.thinkorswim.com/center/howToTos/thinkManual/Trade/Active-Trader/AT-Entering-Orders
-        // Stop loss is set to 25% for all contracts (we use 30% for slippage / market orders)
-        // Take profits are set at 75%, 100%, and 125% which gives us around a 25% - 100% ROC
-
-        // We start with an account size of $250. Increasing this initial balance doesn't improve results (actually makes it worse)
-        // Anytime we reach a new profitLevel (e.g. 1000, 2000, etc.) we take half of that value and add permanently to our bank-roll
-        // After reaching this profitLevel, we increase our level to the next. (e.g. tradeBalance hit 1000, now we target 2000)
-
-        // The only time we return to a previous level is when the tradeBalance falls below $80. This is where we have
-        // to add more money from our bank-roll. We always only add $250 as this tradeBalance is very volatile.
-        // This protects our accounts from the inevitable draw-downs. Worst case scenario we lose 10 trades in a row
-        // and lose $250. However, we still have a massive bank-roll that will supply us with unlimited $250 initial balances.
-        runSimulation(40, 0.25, 1.00, 0.25, 0.30, true);
+    static void loseUntilZero() {
+        int count = 1;
+        double balance = 250;
+        while (balance > 100) {
+            balance = (balance * 0.80);
+            try {
+                TimeUnit.MILLISECONDS.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("[" + count + "]" + printBalance(balance));
+            count++;
+        }
     }
 
-    static void runManySimulations() {
-        for (int w = 30; w <= 50; w += 2) {
+    static void simulateStockStrategy() {
+        runSimulation(25000, 35, 0.03, 0.09, 0.01, 0.03, false, true);
+    }
+
+    static void simulateOptionsStrategy() {
+        runSimulation(250, 34, 0.15, 0.85, 0.10, 0.25, true, true);
+    }
+
+    static void simulateCryptoStrategy() {
+        runSimulation(300, 45, 0.50, 0.60, 0.15, 0.20, true, true);
+    }
+
+    static void runManySimulations(double initialBalance, double minWin, double maxWin, double minLoss, double maxLoss, boolean useBankRoll) {
+        double riskPerTrade = (minLoss + maxLoss) / 2;
+        double rewardPerTrade = (minWin + maxWin) / 2;
+        double riskRewardRatio = Math.round(rewardPerTrade / riskPerTrade);
+        System.out.println("Initial Balance: " + printBalance(initialBalance) + " | AVG Risk: " + printPercentage(riskPerTrade) + " | AVG Reward: " + printPercentage(rewardPerTrade) + " | Ratio: " + riskRewardRatio + " | Use Bankroll: " + useBankRoll);
+        for (int w = 20; w <= 40; w += 2) {
             double totalBalance = 0;
             int numberTrials = 100;
             for (int i = 0; i < numberTrials; i++) {
-                double finalBalance = runSimulation(w, 0.25, 1.00, 0.25, 0.30, false);
+                double finalBalance = runSimulation(initialBalance, w, minWin, maxWin, minLoss, maxLoss, useBankRoll, false);
                 totalBalance += finalBalance;
             }
             double averageBalance = totalBalance / numberTrials;
@@ -39,35 +72,36 @@ public class Simulation {
         }
     }
 
-    static double runSimulation(int winRate, double minWin, double maxWin, double minLoss, double maxLoss, boolean logResults) {
+    static double runSimulation(double initialBalance, int winRate, double minWin, double maxWin, double minLoss, double maxLoss, boolean useBankRoll, boolean logResults) {
         Random random = new Random();
         int tradeCount = 1;
-        final double initialBalance = 250;
         double tradeBalance = initialBalance;
         double bankBalance = 0;
         final double commission = 0.65;
         final double avgContractCost = 80;
         double totalCommissionsPaid = 0;
         double totalLossesIncurred = 0;
+        double totalWinsAccumulated = 0;
         int level = 0;
         while (tradeCount <= 500) {
-            if (tradeBalance > 10000000 || bankBalance > 10000000) {
+            if (bankBalance > 10000000 || tradeBalance > 10000000) {
                 if (logResults) {
                     System.out.println("Terminated after " + tradeCount + " trades.");
                 }
                 break;
             }
-            int randomScore = random.nextInt(100);
-            boolean isWinningTrade = randomScore <= winRate;
+            int score = random.nextInt(100);
+            boolean isWinningTrade = score <= winRate;
             String bankBalanceString = printBalance(bankBalance);
             if (isWinningTrade) {
-                double profitPercent = random.nextDouble(maxWin - minWin) + minWin;
-                double newTradeBalance = Math.round(tradeBalance * (1 + profitPercent));
-                tradeBalance = (tradeBalance * profitPercent) + tradeBalance;
+                double percentGain = random.nextDouble(maxWin - minWin) + minWin;
+                double netGain = (tradeBalance * percentGain);
+                tradeBalance += netGain;
+                totalWinsAccumulated += netGain;
                 if (logResults) {
-                    System.out.println("[" + tradeCount + "] W | Trade Balance: " + printBalance(newTradeBalance, profitPercent) + " | Bank Balance: " + bankBalanceString + " | Score: " + randomScore + " | Level: " + level + " | Commissions: " + printBalance(totalCommissionsPaid) + " | Total Losses: " + printBalance(totalLossesIncurred));
+                    printTrade(tradeCount, netGain, percentGain, tradeBalance, bankBalance, score, level);
                 }
-                if (tradeBalance > profitLevels[level]) {
+                if (useBankRoll && tradeBalance > profitLevels[level]) {
                     bankBalance += profitLevels[level] / 2;
                     tradeBalance -= profitLevels[level] / 2;
                     if (level < profitLevels.length - 1) {
@@ -75,16 +109,17 @@ public class Simulation {
                     }
                 }
             } else {
-                double profitPercent = -1 * (random.nextDouble(maxLoss - minLoss) + minLoss);
-                double tradeLoss = Math.round(tradeBalance * profitPercent);
-                tradeBalance += tradeLoss;
-                totalLossesIncurred += tradeLoss;
+                double percentGain = -1 * (random.nextDouble(maxLoss - minLoss) + minLoss);
+                double netGain = Math.round(tradeBalance * percentGain);
+                tradeBalance += netGain;
+                totalLossesIncurred += netGain;
                 if (logResults) {
-                    System.out.println("[" + tradeCount + "] L | Trade Balance: " + printBalance(tradeBalance, profitPercent) + " | Bank Balance: " + bankBalanceString + " | Score: " + randomScore + " | Level: " + level + " | Commissions: " + printBalance(totalCommissionsPaid) + " | Total Losses: " + printBalance(totalLossesIncurred));
+                    printTrade(tradeCount, netGain, percentGain, tradeBalance, bankBalance, score, level);
                 }
                 if (tradeBalance < 80) {
-                    bankBalance -= initialBalance;
-                    tradeBalance = initialBalance;
+                    double amtToDeposit = initialBalance - tradeBalance;
+                    bankBalance -= amtToDeposit;
+                    tradeBalance += amtToDeposit;
                     if (level > 1) {
                         level--;
                     }
@@ -96,7 +131,7 @@ public class Simulation {
             tradeCount++;
             if (logResults) {
                 try {
-                    TimeUnit.MILLISECONDS.sleep(250);
+                    TimeUnit.MILLISECONDS.sleep(1000);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -108,9 +143,25 @@ public class Simulation {
         if (logResults) {
             System.out.println("===========================================================");
             System.out.println("Final Balance: " + printBalance(finalBalance) + " | After Taxes: " + printBalance(finalBalanceAfterTax) + " | Taxes Paid: " + printBalance(taxesPaid) + " | ROIC: " + calculateROIC(initialBalance, finalBalanceAfterTax));
+            System.out.println("Total Wins: " + printBalance(totalWinsAccumulated) + " | Total Losses: " + printBalance(totalLossesIncurred) + " Total Commissions: " + printBalance(totalCommissionsPaid));
             System.out.println("===========================================================");
         }
-        return finalBalance;
+        return finalBalanceAfterTax;
+    }
+
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    static void printTrade(int tradeCount, double netGain, double percentGain, double tradeBalance, double bankBalance, double score, double level) {
+        String textColor = netGain > 0 ? ANSI_GREEN : ANSI_RED;
+        String tradeCountStr = "[" + tradeCount + "] " + (netGain > 0 ? ANSI_GREEN + "W" + ANSI_RESET : ANSI_RED + "L" + ANSI_RESET);
+        String netGainStr = "Trade Gain: " + printBalance(netGain, percentGain);
+        String tradeBalanceStr = "Trade Balance: " + printBalance(tradeBalance);
+        String bankBalanceStr = "Bank Balance: " + printBalance(bankBalance);
+        String scoreStr = "Score: " + score;
+        String levelStr = "Level: " + level;
+        System.out.format("%8s%3s%34s%3s%28s%3s%28s%3s%12s%3s%12s%1s", tradeCountStr, " | ", netGainStr, " | ", tradeBalanceStr, " | ", bankBalanceStr, " | ", scoreStr, " | ", levelStr, "\n");
     }
 
     static String printBalance(double balance) {
@@ -122,7 +173,13 @@ public class Simulation {
         NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
         NumberFormat percentFormat = NumberFormat.getPercentInstance();
         String balanceString = currencyFormat.format(balance);
-        return balanceString + " (" + percentFormat.format(percentProfit) + ")";
+        String plusStr = balance > 0 ? ANSI_GREEN + "+" : ANSI_RED + "";
+        return plusStr + balanceString + " (" + plusStr + percentFormat.format(percentProfit) + ")" + ANSI_RESET;
+    }
+
+    static String printPercentage(double percent) {
+        NumberFormat percentFormat = NumberFormat.getPercentInstance();
+        return percentFormat.format(percent);
     }
 
     static String calculateROIC(double initialBalance, double finalBalance) {
